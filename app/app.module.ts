ad8b031a2354d4d80bda4434535bbe24e9da8ba1
jwaterworth <j.waterworth1990@gmail.com>
2016-09-03 13:36:18 +0100

    Add http mocks

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,21 +1,30 @@
 import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import './rxjs-extensions';
 
 import { AppComponent } from './app.component';
 import { HeroService } from './hero.service';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent} from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component';
 import { routing } from './app.routing';
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
         routing
         ],
-    declarations:   [AppComponent, HeroesComponent, HeroDetailComponent, DashboardComponent],
+    declarations:   [AppComponent, HeroesComponent, HeroDetailComponent, DashboardComponent, HeroSearchComponent],
     providers:      [HeroService],
     bootstrap:      [AppComponent]
 })
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -40,4 +40,23 @@ export class HeroesComponent implements OnInit {
     public goToDetail(): void {
         this.router.navigate(['/detail', this.selectedHero.id]);
     }
+
+    public add(name: string): void {
+        name = name.trim();
+        if (!name) { return; }
+        this.heroService.create(name)
+            .then(hero => {
+                this.heroes.push(hero);
+                this.selectedHero = null;
+            });
+    }
+
+    public delete(hero: Hero): void {
+        this.heroService
+            .delete(hero.id)
+            .then(() => {
+                this.heroes = this.heroes.filter(h => h !== hero);
+                if (this.selectedHero === hero) { this.selectedHero = null; }
+            })
+    }
 }
